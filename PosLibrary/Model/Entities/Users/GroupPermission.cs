9555f6c16e5267b7011dd5650f8affb57dc4727a
Jaury Nicolas Abreu de Leon <jauryabreu@gmail.com>
2021-08-05 23:14:46 -0400

    First Commit

--- PosLibrary/Model/Entities/Users/GroupPermission.cs
+++ PosLibrary/Model/Entities/Users/GroupPermission.cs
@@ -2,8 +2,8 @@
 {
     public class GroupPermission : CommonEntity
     {
-        public int UserGroupId { get; set; }
-        public int PermissionId { get; set; }
+        public int UserGroupId { get; set; } = 1;
+        public int PermissionId { get; set; } = 1;
 
         public virtual Permission Permission { get; set; }
         public virtual UserGroup UserGroup { get; set; }
