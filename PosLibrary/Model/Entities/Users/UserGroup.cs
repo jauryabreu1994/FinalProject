5b0a738b06293e3c06bfbcd27b28a8f20924aced
Jaury Nicolas Abreu de Leon <jauryabreu@gmail.com>
2021-08-05 23:22:14 -0400

    First Commit

--- PosLibrary/Model/Entities/Users/UserGroup.cs
+++ PosLibrary/Model/Entities/Users/UserGroup.cs
@@ -4,7 +4,7 @@ namespace PosLibrary.Model.Entities.User
 {
     public class UserGroup : CommonEntity
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = "";
         public virtual ICollection<User> Users { get; set; }
         public virtual ICollection<GroupPermission> GroupPermissions { get; set; }
     }
