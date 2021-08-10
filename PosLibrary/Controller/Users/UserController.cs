2f10cfcbb66ba52258f06c89ffa12994b2a00d3c
Rsantiago24 <rickysantiago24.rs@gmail.com>
2021-08-10 12:26:45 -0400

    UserController (Completo)

--- PosLibrary/Controller/Users/UserController.cs
+++ PosLibrary/Controller/Users/UserController.cs
@@ -1,10 +1,12 @@
 ﻿using PosLibrary.Model.Context;
+using PosLibrary.Model.Entities;
 using PosLibrary.Model.Entities.User;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace PosLibrary.Controller.Users
 {
@@ -28,10 +30,47 @@ namespace PosLibrary.Controller.Users
                 var lines = ctx.User.Where(a => !a.Deleted && a.Condition_Status).ToList();
 
                 return lines;
-
-
             }
 
         }
+
+        public OperationResult Update (int ID)
+        {
+
+            using (MainDbContext ctx = new MainDbContext())
+            {
+
+                ctx.Entry(ID).State = EntityState.Modified;
+                
+                ctx.SaveChanges();
+
+                return new OperationResult() { Success = true};
+            }
+
+        }
+
+        public User Create(User user) 
+        {
+            using (MainDbContext ctx = new MainDbContext())
+            {
+                ctx.Entry(user).State = EntityState.Added;
+
+                return user;
+            }
+        }
+
+        public OperationResult Delete(User user)
+        {
+            using (MainDbContext ctx = new MainDbContext())
+            {
+                ctx.User.Remove(user);
+
+                ctx.SaveChanges();
+
+                return new OperationResult() { Success = true };
+
+            }
+        }
+
     }
 }
